4c3e9c46962af7827ab45035d2e76eef34014869
SayedulAlamK21050693 <sayedulalam123@gmail.com>
2022-10-13 12:26:30 +0100

    Update median.py

--- median.py
+++ median.py
@@ -2,11 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 while True:
-    try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
-    except ValueError:
-        print("Some input could not be converted to a number!")
-    else:
-        break
-print(numbers)
+3	    try:
+4	        print("Enter a list of numbers separated by commas: ")
+5	        numbers = [float(value) for value in input().split(",")]
+6	    except ValueError:
+7	        print("Some input could not be converted to a number!")
+8	    else:
+9	        sizeOfNumbers = len(numbers)
+10	        if (sizeOfNumbers % 2 == 0):
+11	            median = (numbers[(sizeOfNumbers // 2) ] + numbers[(sizeOfNumbers // 2) - 1]) / 2
+12	            print(median)
+13	        else:
+14	            median = numbers[sizeOfNumbers//2]
+15	            print(median)
+16	        print(numbers)
